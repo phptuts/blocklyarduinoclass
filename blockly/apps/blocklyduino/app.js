65dc1dcf98e9b68e352b4c89be205b733cbcdf87
phptuts <noah.php@gmail.com>
2017-10-15 17:42:24 -0500

    Got Node Js Version working

--- blockly/apps/blocklyduino/app.js
+++ blockly/apps/blocklyduino/app.js
@@ -1,57 +1,57 @@
 var SerialPort = require('SerialPort');
-var RX = require('rxjs');
+
 var os = require('os');
 var cmd = require('node-cmd');
 var fs = require('fs');
 
-var ports = null;
+var RX = require('rxjs');
+require('rxjs/Operator/distinctUntilChanged');
 
-var serialPortBehaviorSubject = new RX.BehaviorSubject(ports);
+
+var serialPortBehaviorSubject = new RX.BehaviorSubject([]);
 var observableUSBPorts = serialPortBehaviorSubject.asObservable();
 
 
-setInterval(function () {
-    SerialPort.list()
-        .then(function (newPorts) {
-            if (JSON.stringify(ports) !== JSON.stringify(newPorts)) {
-                ports = newPorts;
-                serialPortBehaviorSubject.next(ports);
-            }
-        })
-        .catch(function (error) {
-            serialPortBehaviorSubject.error(error);
-        });
+setInterval(() => {
+    SerialPort
+        .list()
+        .then(newPorts => serialPortBehaviorSubject.next(newPorts))
+        .catch(error => serialPortBehaviorSubject.error(error));
 }, 100);
 
 observableUSBPorts
-    .subscribe(val => console.log('USB:', val));
-
-var code = `void setup()
-{
-  pinMode(13, OUTPUT);
-}
+    .distinctUntilChanged(null, (ports) => ports.length)
+    .subscribe(usbPorts => console.log('USB:', usbPorts));
 
 
-void loop()
-{
-   digitalWrite(13, HIGH);
-   delay(100);
-   digitalWrite(13, LOW);
-   delay(100);
-}`;
+let uploadCode = () => {
+    var code = `void setup()
+                {
+                  pinMode(13, OUTPUT);
+                }
+                
+                
+                void loop()
+                {
+                   digitalWrite(13, HIGH);
+                   delay(100);
+                   digitalWrite(13, LOW);
+                   delay(100);
+                }`;
 
-if (os.platform() == 'darwin') {
+    if (os.platform() == 'darwin') {
 
-    if (fs.existsSync('sketch.ino')) {
-        fs.unlinkSync('sketch.ino');
-    }
-    fs.writeFileSync('sketch.ino', code);
+        if (fs.existsSync('sketch.ino')) {
+            fs.unlinkSync('sketch.ino');
+        }
+        fs.writeFileSync('sketch.ino', code);
 
-    var arduinoFile = '/Applications/Arduino.app/Contents/MacOS/Arduino';
-    cmd.get(`
+        var arduinoFile = '/Applications/Arduino.app/Contents/MacOS/Arduino';
+        cmd.get(`
         ${arduinoFile} --upload sketch.ino --port /dev/tty.usbmodem1411
-    `, function (err, data, stderr) {
-        fs.unlinkSync('sketch.ino');
-        console.log(err);
-    });
-}
+    `, (err, data, stderr) => {
+            fs.unlinkSync('sketch.ino');
+            console.log(err);
+        });
+    }
+};
